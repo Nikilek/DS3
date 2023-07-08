e958316721ee518fa7bce6276f70da158f219e28
Nika <Vika_av@bk.ru>
2023-07-08 19:07:16 +0300

    домашняя работа 3

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿void Task1()
+/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+{
+    Console.WriteLine("Введите пятизначное число: ");
+    string numbers = Console.ReadLine();
+    int len = numbers.Length;
+
+    if (len == 5)
+    {
+        if (numbers[0] == numbers[4] && numbers[1] == numbers[3])
+        {
+            Console.WriteLine($"{numbers} - полиндром");
+        }
+        else
+        {
+            Console.WriteLine($"{numbers} - не полиндром");
+        }
+    }
+    else
+    {
+        Console.WriteLine($" Ошибка: {numbers} - не являеться пятизначным");
+    }
+}
+
+
+void Task2()
+/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+{
+    int x1 = ReadInt("Введите координату X первой точки:");
+    int y1 = ReadInt("Введите координату Y первой точки:");
+    int z1 = ReadInt("Введите координату Z первой точки:");
+    int x2 = ReadInt("Введите координату X второй точки:");
+    int y2 = ReadInt("Введите координату Y второй точки:");
+    int z2 = ReadInt("Введите координату Z второй точки:");
+
+    int A = x2 - x1;
+    int B = y2 - y1;
+    int C = z1 - z2;
+
+    double Length = Math.Sqrt(A * A + B * B + C * C);
+    Console.WriteLine($"Длина отрезка {Length}");
+
+    int ReadInt(string message)
+    {
+        Console.Write(message);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+
+void Task3()
+/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+{
+    int numbers = ReadInt("Введите число N: ");
+    for (int i = 1; i < numbers; i++)
+    {
+        Console.WriteLine($"{i*i*i}");
+    }
+    int ReadInt(string message)
+    {
+        Console.Write(message);
+        return Convert.ToInt32(Console.ReadLine());
+}
+}
+Task3();
